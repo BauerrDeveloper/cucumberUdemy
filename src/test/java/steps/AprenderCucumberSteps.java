e1ceec3a0842311a7db1a3de50549243f1426982
Rodrigo Paulo <ro.bauerr@gmail.com>
2022-03-30 14:30:14 -0300

    Second commit.

--- src/test/java/steps/AprenderCucumberSteps.java
+++ src/test/java/steps/AprenderCucumberSteps.java
@@ -5,9 +5,15 @@ import cucumber.api.java.pt.Então;
 import cucumber.api.java.pt.Quando;
 import org.junit.Assert;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 public class AprenderCucumberSteps {
 
     private int contador = 0;
+    Date entrega = new Date();
 
     @Dado("^que criei o arquivo corretamente$")
     public void queCrieiOArquivoCorretamente() {
@@ -25,19 +31,19 @@ public class AprenderCucumberSteps {
     }
 
     @Dado("^que o valor do contador é (\\d+)$")
-    public void queOValorDoContador(int arg1) throws Throwable {
+    public void queOValorDoContador(int arg1) {
         contador = arg1;
         System.out.println("Valor do contador: " + contador);
     }
 
     @Quando("^eu incrementar em (\\d+)$")
-    public void euIncrementarEm(int arg1) throws Throwable {
+    public void euIncrementarEm(int arg1) {
         contador += arg1;
         System.out.println("Valor do contador: " + contador);
     }
 
     @Então("^o valor do contador será (\\d+)$")
-    public void oValorDoContadorSera(int arg1) throws Throwable {
+    public void oValorDoContadorSera(int arg1) {
         if (contador == 18) {
             System.out.println("Soma correta!");
         } else {
@@ -45,19 +51,32 @@ public class AprenderCucumberSteps {
         }
     }
 
-
-    @Dado("^que acesso a home do portal puravida prime$")
-    public void queAcessoAHomeDoPortalPuravidaPrime() {
-        System.out.println("Acessando o endereço https://app-prime.azurewebsites.net");
+    @Dado("^que a entrega dve ser feita no dia (\\d+)/(\\d+)/(\\d+)$")
+    public void queAEntregaDveSerFeitaNoDia(int dia, int mes, int ano) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.DAY_OF_MONTH, dia);
+        calendar.set(Calendar.MONTH, mes - 1);
+        calendar.set(Calendar.YEAR, ano);
+        entrega = calendar.getTime();
     }
 
-    @Quando("^realizo login$")
-    public void realizoLogin() {
-        System.out.println(" Clicando no botão login\n Inserindo e-mail e senha\n Clicando em entrar");
+    @Quando("^a entrega atrasar em (\\d+) (dia|dias|mes|meses)$")
+    public void aEntregaAtrasarEmDias(int arg0, String tempo) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(entrega);
+        if (tempo.equals("dias")) {
+            calendar.add(Calendar.DAY_OF_MONTH, arg0);
+        }
+        if (tempo.equals("meses")) {
+            calendar.add(Calendar.MONTH, arg0);
+        }
+        entrega = calendar.getTime();
     }
 
-    @Então("^sou redirecionado para a home$")
-    public void souRedirecionadoParaAHome() {
-        System.out.println("Estou na home logada do portal");
+    @Então("^a entrega deve ser efetuada dia (\\d{2}/\\d{2}/\\d{4})$")
+    public void aEntregaDeveSerEfetuadaDia(String data) {
+        DateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+        String dataFormatada = format.format(entrega);
+        Assert.assertEquals(data, dataFormatada);
     }
 }
--- src/test/resources/features/aprender_cucumber.feature
+++ src/test/resources/features/aprender_cucumber.feature
@@ -1,17 +1,25 @@
 # language: pt
-  Funcionalidade: : Aprender cucumber
+  Funcionalidade: Aprender cucumber
+    Como um aluno
+    Eu quero aprender a utilizar o Cucumber
+    Para que eu possa automatizar critérios de aceitação
+
     Cenario: Deve executar especificação
       Dado que criei o arquivo corretamente
       Quando executa-lo
       Então a especificacao deve finalizar com sucesso
-
 
     Cenario: Deve incrementar contador
       Dado que o valor do contador é 15
       Quando eu incrementar em 3
       Então o valor do contador será 18
 
-     Cenario: Login com dados validos
-       Dado que acesso a home do portal puravida prime
-       Quando realizo login
-       Então sou redirecionado para a home
+    Cenario: Deve calcular atraso na entrega
+      Dado que a entrega dve ser feita no dia 05/04/2018
+      Quando a entrega atrasar em 2 dias
+      Então a entrega deve ser efetuada dia 07/04/2018
+
+    Cenario: Deve calcular atraso na entrega da China
+      Dado que a entrega dve ser feita no dia 05/04/2018
+      Quando a entrega atrasar em 2 meses
+      Então a entrega deve ser efetuada dia 05/06/2018
